f004f47996b4f0fbaf761f3070f8ed375ac974fc
Max Phua <maxphua90@gmail.com>
2024-05-23 08:58:56 +0800

    Implement recent transaction

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Session } from 'inspector'
+import Link from 'next/link'
+import { BankTabItem } from './BankTabItem'
+import BankInfo from './BankInfo'
+import TransactionsTable from './TransactionsTable'
 
 
 const RecentTransactions = ({
@@ -11,14 +15,52 @@ const RecentTransactions = ({
 }: RecentTransactionsProps) => {
   return (
     <section>
-        <Tabs defaultValue="account" className="w-[400px]">
-            <TabsList>
-                <TabsTrigger value="account">Account</TabsTrigger>
-                <TabsTrigger value="password">Password</TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">Make changes to your account here.</TabsContent>
-            <TabsContent value="password">Change your password here.</TabsContent>
-        </Tabs>
+        <header className="flex items-center justify-between">
+        <h2 className="recent-transactions-label">Recent transactions</h2>
+        <Link
+          href={`/transaction-history/?id=${appwriteItemId}`}
+          className="view-all-btn"
+        >
+          View all
+        </Link>
+      </header>
+
+      <Tabs defaultValue={appwriteItemId} className="w-full">
+      <TabsList className="recent-transactions-tablist">
+          {accounts.map((account: Account) => (
+            <TabsTrigger key={account.id} value={account.appwriteItemId}>
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
+              />
+            </TabsTrigger>
+          ))}
+        </TabsList>
+
+        {accounts.map((account: Account) => (
+          <TabsContent
+            value={account.appwriteItemId}
+            key={account.id}
+            className="space-y-4"
+          >
+            <BankInfo 
+              account={account}
+              appwriteItemId={appwriteItemId}
+              type="full"
+            />
+
+            <TransactionsTable transactions={currentTransactions} />
+            
+
+            {totalPages > 1 && (
+              <div className="my-4 w-full">
+                <Pagination totalPages={totalPages} page={page} />
+              </div>
+            )}
+          </TabsContent>
+        ))}
+      </Tabs>
     </section>
     
 
